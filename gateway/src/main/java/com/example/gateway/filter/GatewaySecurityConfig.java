12346d8eb8e29cefa4220f80f0519d36ac0bda77
tranchi0120 <tranchi20012000@gmail.com>
2024-01-21 14:35:01 +0700

    done gateway filter

--- gateway/src/main/java/com/example/gateway/filter/GatewaySecurityConfig.java
+++ gateway/src/main/java/com/example/gateway/filter/GatewaySecurityConfig.java
@@ -1,38 +1,29 @@
-//package com.example.gateway.filter;
-//
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.http.HttpMethod;
-//import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
-//import org.springframework.security.config.web.server.ServerHttpSecurity;
-//import org.springframework.security.web.server.SecurityWebFilterChain;
-//
-//@Configuration
-//@EnableWebFluxSecurity
-//public class GatewaySecurityConfig {
-//
-//    @Bean
-//    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
-//        http
-//                .authorizeExchange(exchanges -> exchanges
-//                        .pathMatchers(HttpMethod.GET, "/api/users/**").permitAll()
-//                        .anyExchange().authenticated()
-//                );
-//        return http.build();
-//    }
-//
-////    @Bean
-////    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
-////        http
-////                .authorizeExchange(exchanges -> exchanges
-////                        // Cho phép tất cả các yêu cầu POST đến /api/users/** không cần xác thực
-////                        .pathMatchers(HttpMethod.POST, "/api/users/**").permitAll()
-////                        // Yêu cầu xác thực cho tất cả các yêu cầu khác
-////                        .anyExchange().authenticated()
-////                )
-////
-////        ;
-////
-////        return http.build();
-////    }
-//}
+package com.example.gateway.filter;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+@EnableWebFluxSecurity
+public class GatewaySecurityConfig {
+
+    @Bean
+    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
+        http
+                .authorizeExchange(exchanges -> exchanges
+                        .pathMatchers(HttpMethod.GET, "/api/users","/api/posts","/api/comments").permitAll()
+                        .pathMatchers(HttpMethod.GET, "/api/users/{id}","/api/posts/{id}","/api/comments/{id}").permitAll()
+                        .pathMatchers(HttpMethod.POST, "/api/users/addUser","/api/posts/addPost", "/api/comments/addComment").permitAll()
+                        .pathMatchers(HttpMethod.PUT, "/api/users/update/{id}","/api/posts/update/{id}", "/api/comments/{id}").permitAll()
+                        .pathMatchers(HttpMethod.DELETE, "/api/users/delete/{id}", "/api/posts/delete/{id}", "/api/posts/delete/{id}").permitAll()
+                        .pathMatchers("/api/users/**","/api/posts/**","/api/comments/**").denyAll()
+                        .anyExchange().authenticated()
+                )
+                .csrf(ServerHttpSecurity.CsrfSpec::disable);
+        return http.build();
+    }
+}
